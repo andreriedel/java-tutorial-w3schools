365bef728f491bf5be0f1b56b7ad9956d5d657f9
André Riedel <andreriedelz@gmail.com>
2026-02-09 00:01:16 -0300

    add 4-comments

---
+++ 4-comments/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.println("Hello Word");
+
+        /* Multi-line comments
+        in Java*/
+    }
+}

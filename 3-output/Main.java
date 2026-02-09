b92b6f2e3d5937815559719118ff263430a9a990
André Riedel <andreriedelz@gmail.com>
2026-02-08 23:00:54 -0300

    add 3-output

---
+++ 3-output/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello Word! ");
+        System.out.println("I'm learning Java");
+        System.out.println(2 * 5);
+    }
+}
